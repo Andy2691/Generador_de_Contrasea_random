f9be27d5c279ba21671835a1dbc616c3e1b47032
91andres <91andresmunoz@gmail.com>
2024-07-02 04:39:58 -0500

    Documentado el código del generador de contraseñas

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# Generador_de_Contrasea_random
+# Generador de Contraseñas Aleatorias
+
+Este proyecto es un generador de contraseñas aleatorias en Python. Utiliza letras, dígitos y caracteres de puntuación para crear contraseñas seguras de una longitud definida por el usuario.
+
+## Descripción
+
+El script genera una contraseña aleatoria basada en la longitud proporcionada por el usuario. Utiliza los módulos `string` y `random` de Python para seleccionar al azar caracteres de un conjunto que incluye letras mayúsculas y minúsculas, dígitos y caracteres de puntuación.
+
+## Uso
+
+1. Clona este repositorio en tu máquina local:
+
+   ```sh
+   git clone https://github.com/tu_usuario/tu_repositorio.git
+   cd tu_repositorio
+   ```
+
+2. Ejecuta el script:
+
+   ```sh
+   python generador_contraseñas.py
+   ```
+
+3. Ingresa la longitud deseada para la contraseña cuando se te pida.
+
+## Ejemplo
+
+```sh
+$ python generador_contraseñas.py
+Cual es la longitud de la contraseña?: 12
+La nueva contraseña es: aB3!eD4#hI5@jK
+```
--- generador.py
+++ generador.py
@@ -1,18 +1,27 @@
-#Generador de contraseñas de manera randomica 
-import string
-import random 
+# Generador de contraseñas de manera aleatoria
 
+# Importamos los módulos necesarios
+import string  # Este módulo contiene varios conjuntos de caracteres (letras, dígitos, puntuación, etc.)
+import random  # Este módulo se utiliza para realizar operaciones aleatorias
 
+# Definimos una función que genera una contraseña
 def generar_contraseña(longitud):
-    caracteres = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!#$%&'*+,-."
+    # Concatenamos letras (mayúsculas y minúsculas), dígitos y caracteres de puntuación en una sola cadena
+    caracteres = string.ascii_letters + string.digits + string.punctuation
+    # Inicializamos una cadena vacía para almacenar la contraseña generada
     contraseña = ""
 
-    for i  in range(longitud):
+    # Iteramos 'longitud' veces para generar cada carácter de la contraseña
+    for i in range(longitud):
+        # Elegimos un carácter al azar de la cadena 'caracteres' y lo añadimos a 'contraseña'
         contraseña += random.choice(caracteres)
+    # Devolvemos la contraseña generada
     return contraseña
 
-longitud = int(input("Cual es la longitud de la contraseña?: ") ) 
+# Solicitamos al usuario que ingrese la longitud deseada para la contraseña
+longitud = int(input("Cual es la longitud de la contraseña?: "))
 
-
+# Generamos una nueva contraseña llamando a la función con la longitud proporcionada
 nueva_contraseña = generar_contraseña(longitud)
-print("La nueva contraseña es : ", nueva_contraseña)
+# Imprimimos la contraseña generada
+print("La nueva contraseña es: ", nueva_contraseña)
